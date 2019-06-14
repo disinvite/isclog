e18e712104daaf5487c5264e4a345f8133c9d1bf
MS <disinvite@users.noreply.github.com>
2019-06-14 15:51:38 -0400

    database

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const fs = require('fs');
 const { XMLParse } = require('./lib/xml');
 const data = fs.readFileSync('./dummy/isclog-cutdown.xml');
+const low = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
+
+const unwrapLB = arr => {
+  const [level, category, msg, pid, namespace,
+    timestamp, routine, requestId, tag] = arr;
+  return {category, msg, pid, namespace, timestamp, routine, requestId, tag};
+};
 
 const xml = new XMLParse();
+const adapter = new FileAsync('./dummy/db.json');
+low(adapter).then(db => {
+  xml.on('size', size => console.log(size));
+  xml.on('$lb', arr => {
+    const obj = unwrapLB(arr);
+    console.log(obj);
+    db.get('entries').push(obj).write();
+  });
+  db.defaults({ entries: [] }).write();
+}).then(() => {
+  xml.write(data);
+});
 
-const isclog = [];
-xml.on('size', size => console.log(size));
-xml.on('$lb', arr => isclog.push({data: arr, sub: []}));
-xml.on('sub', str => isclog[isclog.length - 1].sub.push(str));
-xml.on('done', () => console.log(isclog));
-xml.write(data);
